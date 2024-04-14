bb69cd058ff5ee3b08cdf9f76ec190be328b24ae
Ahmed Abdullah <ahmadabdullah4919@gmail.com>
2024-04-14 11:36:22 +0500

    Refactor utils.js to add slugify and unslugify functions, and update domain constant

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,27 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export function slugify(text) {
+  return text
+    .toString()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/--+/g, "-");
+}
+
+export function unslugify(text) {
+  return text
+    .toString()
+    .replace(/-/g, " ")
+    .replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase());
+}
+
+export const domain = "https://countries-front-end-v2-six.vercel.app";
